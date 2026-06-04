7bdec01a31e51a99bcb778c8a6f26e8c2e42d407
YK <yockkang@gmail.com>
2026-06-04 18:50:20 +0800

    Added Upgrade Framework

---
+++ core/src/main/java/com/main/CoreWorks/Factory/Upgrade/BufferSizeUpgrade.java
@@ -0,0 +1,20 @@
+package com.main.CoreWorks.Factory.Upgrade;
+
+import com.main.CoreWorks.Factory.Building;
+import com.main.CoreWorks.Factory.Shooter;
+
+public class BufferSizeUpgrade extends UpgradeAspect{
+    public BufferSizeUpgrade(int value) {
+        super((float) value);
+    }
+
+    @Override
+    public void execute(Building b) {
+        b.changeCapacityMult((int) value);
+    }
+
+    @Override
+    public boolean tryExecute(Building b) {
+        return true;
+    }
+}
