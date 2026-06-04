afbf656fa510a0db4663d698f786ed891c9179e7
YK <yockkang@gmail.com>
2026-06-04 18:50:20 +0800

    Added Upgrade Framework

---
+++ core/src/main/java/com/main/CoreWorks/Factory/Upgrade/BaseDamageUpgrade.java
@@ -0,0 +1,26 @@
+package com.main.CoreWorks.Factory.Upgrade;
+
+import com.main.CoreWorks.Factory.Building;
+import com.main.CoreWorks.Factory.Shooter;
+
+public class BaseDamageUpgrade extends UpgradeAspect{
+    public BaseDamageUpgrade(float value) {
+        super(value);
+    }
+
+    @Override
+    public void execute(Building b) {
+        if (b instanceof Shooter) {
+            ((Shooter) b).changeBaseDamage(value);
+        }
+    }
+
+    @Override
+    public boolean tryExecute(Building b) {
+        if (b instanceof Shooter) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
---
+++ core/src/main/java/com/main/CoreWorks/Factory/Upgrade/FlatSpeedUpgrade.java
@@ -0,0 +1,20 @@
+package com.main.CoreWorks.Factory.Upgrade;
+
+import com.main.CoreWorks.Factory.Building;
+
+public class FlatSpeedUpgrade  extends UpgradeAspect{
+    public FlatSpeedUpgrade(int value) {
+        super((float) value);
+    }
+
+    @Override
+    public void execute(Building b) {
+        b.addSpeedFlat((int) value);
+    }
+
+
+    @Override
+    public boolean tryExecute(Building b) {
+        return true;
+    }
+}
