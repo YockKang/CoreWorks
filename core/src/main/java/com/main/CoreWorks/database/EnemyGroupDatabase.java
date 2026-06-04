7196890ea0e76324bc3eb01bd3c158569942e509
YK <yockkang@gmail.com>
2026-06-04 17:12:56 +0800

    Added Enemy Groups

---
+++ core/src/main/java/com/main/CoreWorks/database/EnemyGroupDatabase.java
@@ -0,0 +1,52 @@
+package com.main.CoreWorks.database;
+
+import com.badlogic.gdx.utils.*;
+import com.main.CoreWorks.entities.*;
+
+import java.util.Random;
+
+public class EnemyGroupDatabase {
+    private static final Array<Array<Array<EnemyFactory>>> EnemyGroupDB = new Array<>(3);
+
+    public static Array<Enemy> register(JsonValue data) {
+        if (data.isArray()) {
+            data.forEach(com.main.CoreWorks.database.EnemyGroupDatabase::register);
+            return null;
+        } else {
+            int tier = data.getInt("Tier") - 1;
+            String[] enemies = data.get("Enemies").asStringArray();
+            Array<EnemyFactory> arr = new Array<>();
+            for (String e : enemies) {
+                arr.add(EnemyDatabase.getEnemyConstructor(e));
+            }
+            if (EnemyGroupDB.get(tier) == null) {
+                EnemyGroupDB.insert(tier, new Array<>());
+            }
+            EnemyGroupDB.get(tier).add(arr);
+            Array<Enemy> enemiesArr = new Array<>();
+            arr.forEach(enemyFactory -> enemiesArr.add(enemyFactory.of(1)));
+            return enemiesArr;
+        }
+    }
+
+    public static Array<EnemyFactory> getGroupConstructor(int tier, int num) {
+        return EnemyGroupDB.get(tier - 1).get(num);
+    }
+
+    public static Array<Enemy> getGroup(int tier, int num, float multiplier) {
+        Array<Enemy> arr = new Array<>();
+        EnemyGroupDB.get(tier - 1).get(num).forEach(enemyFactory -> arr.add(enemyFactory.of(multiplier)));
+        return arr;
+    }
+
+    public static Array<Enemy> getRandomGroup(int tier, float multiplier, Random random) {
+        Array<Enemy> arr = new Array<>();
+        Array<Array<EnemyFactory>> group = EnemyGroupDB.get(tier - 1);
+        group.get(random.nextInt(group.size)).forEach(enemyFactory -> arr.add(enemyFactory.of(multiplier)));
+        return arr;
+    }
+
+    public static String showDB(){
+        return EnemyGroupDB.toString();
+    }
+}
--- core/src/main/java/com/main/CoreWorks/entities/EnemyFactory.java
+++ core/src/main/java/com/main/CoreWorks/entities/EnemyFactory.java
@@ -4,9 +4,16 @@ import com.badlogic.gdx.utils.*;
 
 public class EnemyFactory {
     JsonValue data;
+    String id;
 
     public EnemyFactory(JsonValue dataIn) {
         data = dataIn;
+        id = data.getString("id");
+    }
+
+    @Override
+    public String toString() {
+        return "Factory of Enemy: " + id;
     }
 
     public Enemy of(float mult) {
